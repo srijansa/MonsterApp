6d9259f05a92d6ad09e66ccd15839dd5397ef2d1
srijansa <159839387+srijansa@users.noreply.github.com>
2025-02-24 23:52:07 -0500

    changed the search component to functional comp

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,27 +1,27 @@
 import './card-list.styles.css';
 
 const CardList = ({monsters}) =>{
-    if (!monsters || !Array.isArray(monsters)) {
-      return <h1>Loading...</h1>; 
-    }
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-        //   <h1 key={monster.id}>{monster.name}</h1>
-            const {name, email, id} = monster;
-            return (
-            <div className="card-container" key={id}>
-                <img 
-                alt={`moster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                />
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-            )
-        })}
-      </div>
-    );
+  if (!monsters || !Array.isArray(monsters)) {
+    return <h1>Loading...</h1>; 
+  }
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+      //   <h1 key={monster.id}>{monster.name}</h1>
+          const {name, email, id} = monster;
+          return (
+          <div className="card-container" key={id}>
+              <img 
+              alt={`moster ${name}`}
+              src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+          </div>
+          )
+      })}
+    </div>
+  );
 }
 
 export default CardList;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,18 +1,15 @@
-import { Component } from "react";
 import './search-box.styles.css'; // this css is not specific to this file, the whole css is picked up and and compiled as a whole 
 
-class SearchBox extends Component{
-    render(){
-        return(
-            <div>
-                <input
-                className="search-box"
-                type="search"
-                placeholder={this.props.placeholder}
-                onChange={this.props.onChangeHandler}
-                />
-            </div>
-        );
-    }
+const SearchBox = () =>{
+    return(
+        <div>
+            <input
+            className="search-box"
+            type="search"
+            placeholder={this.props.placeholder}
+            onChange={this.props.onChangeHandler}
+            />
+        </div>
+    );
 }
 export default SearchBox;
